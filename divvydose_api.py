6a4f6a1432ced07dd092e8518a8d5e8d222f5160
carter3689 <carter3689@gmail.com>
2018-09-02 13:06:59 -0500

    Updates to api

--- divvydose_api.py
+++ divvydose_api.py
@@ -8,6 +8,9 @@ api = Api(app)
 
 g = Github()
 user = requests.get('https://api.github.com/users/kennethreitz')
+watchers = requests.get('https://api.github.com/users/kennethreitz/subscriptions')
+stars = requests.get('https://api.github.com/users/kennethreitz/starred')
+stars_given = requests.get('http://api.github.com/users/starred/kennethreitz/repo')
 repos = requests.get('https://api.github.com/repos')
 total = user.json()
 
@@ -16,7 +19,13 @@ bitbucket = requests.get('https://api.bitbucket.org/2.0/users/tutorials')
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': total["public_repos"], "bitbucket":bitbucket.json(),"forks":repos.json() }
+        return {'data': total["public_repos"],
+        "bitbucket":bitbucket.json(), 
+        "forks":repos.json(), 
+        "watchers":watchers.json(),
+        "stars":stars.json(),
+        "stars-given":stars_given.json()
+        }
 
 
 api.add_resource(HelloWorld, '/')
